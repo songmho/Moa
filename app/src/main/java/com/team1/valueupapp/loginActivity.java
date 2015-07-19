29012a03b2cf789eee633c6bc22a6fd200659ff3
eugene <7yjlove7@naver.com>
2015-07-19 13:15:01 +0900

    login text color 변경

--- app/src/main/java/com/team1/valueupapp/loginActivity.java
+++ app/src/main/java/com/team1/valueupapp/loginActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /**
  * Created by eugene on 2015-07-18.
@@ -14,6 +20,7 @@ public class loginActivity extends ActionBarActivity {
     EditText id_text;
     EditText pass_text;
     Button login_btn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +29,32 @@ public class loginActivity extends ActionBarActivity {
         id_text= (EditText) findViewById(R.id.id_text);
         pass_text= (EditText) findViewById(R.id.pass_text);
         login_btn= (Button) findViewById(R.id.login_btn);
+        progressBar=(ProgressBar)findViewById(R.id.progressbar);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(loginActivity.this, MainActivity.class));
-                finish();
+                //startActivity(new Intent(loginActivity.this, MainActivity.class));
+                //finish();
+
+                ParseUser.logInInBackground(String.valueOf(id_text.getText()), String.valueOf(pass_text.getText()), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user != null){
+                            startActivity(new Intent(loginActivity.this, MainActivity.class));
+                            finish();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"틀렸습니다.",Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                });
+
+
+
+
             }
         });
 
