b078391967cc4885b25d8015194976153dfcbc69
Han-Hm <hyemihan89@gmail.com>
2015-07-23 17:25:37 +0900

    새로운 와이어프레임대로 팀빌딩 리스트변경

--- app/src/main/java/com/team1/valueupapp/Grid_Item.java
+++ app/src/main/java/com/team1/valueupapp/Grid_Item.java
@@ -7,11 +7,8 @@ public class Grid_Item {
     String idea;
     String state;
     int plan;
-    int max_plan;
     int develop;
-    int max_dev;
     int design;
-    int max_dis;
     String idea_info;
 
 
@@ -31,29 +28,16 @@ public class Grid_Item {
         return  this.design;
     }
 
-    int getMax_dev() {
-        return max_dev;
-    }
-    int getMax_dis() {
-        return max_dis;
-    }
-    int getMax_plan() {
-        return max_plan;
-    }
-
     public String getIdea_info() {
         return idea_info;
     }
 
-    public Grid_Item(String idea, String state, int plan, int max_plan, int develop, int max_dev, int design, int max_dis, String idea_info){
+    public Grid_Item(String idea, String state, int plan, int develop, int design, String idea_info){
         this.idea=idea;
         this.state=state;
         this.plan=plan;
-        this.max_plan=max_plan;
         this.develop=develop;
-        this.max_dev=max_dev;
         this.design=design;
-        this.max_dis=max_dis;
         this.idea_info=idea_info;
     }
 }
