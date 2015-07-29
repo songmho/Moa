3a1fbcc2d3bc1a52d9247907b2a5d1e63390ee28
eugene <7yjlove7@naver.com>
2015-07-29 16:16:33 +0900

    설정페이지 로그아웃

--- app/src/main/java/com/team1/valueupapp/SetupFragment.java
+++ app/src/main/java/com/team1/valueupapp/SetupFragment.java
@@ -7,22 +7,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 /**
  * Created by songmho on 2015-07-04.
  */
 public class SetupFragment extends Fragment {
     LinearLayout cur_container;
+    TextView logout;
+    ProgressBar progressBar;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        logout = (TextView) cur_container.findViewById(R.id.logout);
+        progressBar=(ProgressBar)cur_container.findViewById(R.id.progressbar);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.VISIBLE);
+
+                        ParseUser.logOut();
+                        ParseUser currentUser = ParseUser.getCurrentUser();
+
+                    }
+                }).start();
+
+
+            }
+        });
+
+
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        cur_container=(LinearLayout)inflater.inflate(R.layout.fragment_setup,container,false);
+        cur_container = (LinearLayout) inflater.inflate(R.layout.fragment_setup, container, false);
         return cur_container;
     }
 
