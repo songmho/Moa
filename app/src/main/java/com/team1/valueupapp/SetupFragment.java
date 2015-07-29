927b56e8773e351c6fa731095d1ba84c49bd9837
eugene <7yjlove7@naver.com>
2015-07-30 01:12:36 +0900

    설정 페이지 switch 토스트임시표시, aboutpage 웹연결

--- app/src/main/java/com/team1/valueupapp/SetupFragment.java
+++ app/src/main/java/com/team1/valueupapp/SetupFragment.java
@@ -1,15 +1,18 @@
 package com.team1.valueupapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.parse.ParseUser;
@@ -20,6 +23,11 @@ import com.parse.ParseUser;
 public class SetupFragment extends Fragment {
     FrameLayout setup_container;
     LinearLayout logout;
+    Switch swc_login;
+    Switch swc_teambuild;
+    Switch swc_notice;
+    Button btn_aboutpage;
+
 
 
     @Override
@@ -34,6 +42,11 @@ public class SetupFragment extends Fragment {
 
         setup_container = (FrameLayout) inflater.inflate(R.layout.fragment_setup, container, false);
         logout = (LinearLayout) setup_container.findViewById(R.id.logout);
+        swc_login=(Switch) setup_container.findViewById(R.id.swc_login);
+        swc_teambuild=(Switch) setup_container.findViewById(R.id.swc_teambuild);
+        swc_notice=(Switch) setup_container.findViewById(R.id.swc_notice);
+        btn_aboutpage=(Button)setup_container.findViewById(R.id.btn_aboutpage);
+
 
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +61,49 @@ public class SetupFragment extends Fragment {
                 }//end else
             }
         });//OnClickListener
+
+        swc_login.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+
+                if (isChecking)
+                    Toast.makeText(getActivity(), "자동로그인 설정", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getActivity(), "자동로그인 해제", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        swc_teambuild.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+
+                if (isChecking)
+                    Toast.makeText(getActivity(), "팀빌딩알림 설정", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getActivity(), "팀빌딩알림 해제", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        swc_notice.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+
+                if (isChecking)
+                    Toast.makeText(getActivity(), "교육일정알림 설정", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getActivity(), "교육일정알림 해제", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_aboutpage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
         return setup_container;
     }
