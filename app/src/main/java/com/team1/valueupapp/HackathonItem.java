5e261de74171c93f2f20751cb0fb75823c031309
eugene <7yjlove7@naver.com>
2015-08-02 16:45:31 +0900

    해커톤 소개페이지 recyclerview

---
+++ app/src/main/java/com/team1/valueupapp/HackathonItem.java
@@ -0,0 +1,23 @@
+package com.team1.valueupapp;
+
+/**
+ * Created by eugene on 2015-08-02.
+ */
+public class HackathonItem{
+
+    int image;
+    String name;
+
+    public HackathonItem(String name, String color) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/team1/valueupapp/hackathon_Recycler.java
@@ -0,0 +1,32 @@
+package com.team1.valueupapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eugene on 2015-08-02.
+ */
+public class hackathon_Recycler extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hackathon);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.hkt_recyclerview);
+
+        List<HackathonItem> dataItems = new ArrayList<HackathonItem>();
+        dataItems.add(new HackathonItem("ÇØÄ¿Åæ", "ÇØÄ¿Åæ"));
+
+
+        HackathonAdapter myAdapter = new HackathonAdapter(dataItems);
+        recyclerView.setAdapter(myAdapter);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
