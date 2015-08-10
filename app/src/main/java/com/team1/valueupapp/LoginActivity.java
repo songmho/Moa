e35d3dc1d0efb933e95e37f1f70b39ab8e1fede5
songmho <songmho@gmail.com>
2015-08-11 01:37:06 +0900

    로그인 시 사진 가져오게 하는 것 작업중.

--- app/src/main/java/com/team1/valueupapp/LoginActivity.java
+++ app/src/main/java/com/team1/valueupapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.team1.valueupapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
@@ -10,9 +12,16 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.parse.GetDataCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by eugene on 2015-07-18.
@@ -49,6 +58,25 @@ public class LoginActivity extends AppCompatActivity {
                                     public void done(ParseUser user, ParseException e) {
                                         progressBar.setVisibility(View.GONE);
                                         if(user != null){
+                                            ParseFile parse_file=(ParseFile)ParseUser.getCurrentUser().get("profile");
+                                            parse_file.getDataInBackground(new GetDataCallback() {
+                                                @Override
+                                                public void done(byte[] bytes, ParseException e) {
+                                                    Bitmap thum= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                                                    File file=new File("profile.jpg");
+                                                    FileOutputStream fos= null;
+                                                    try {
+                                                        fos = openFileOutput("profile.jpg",0);
+                                                        thum.compress(Bitmap.CompressFormat.JPEG,100,fos);
+                                                        fos.flush();
+                                                        fos.close();
+                                                    } catch (FileNotFoundException e1) {
+                                                        e1.printStackTrace();
+                                                    } catch (IOException e1) {
+                                                        e1.printStackTrace();
+                                                    }
+                                                }
+                                            });
                                             startActivity(new Intent(LoginActivity.this, MainActivity.class));
                                             finish();
                                         }else{
