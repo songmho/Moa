6fdffed6ac0625ec28598479b68f307708f9d9ec
songmho <songmho@gmail.com>
2016-02-27 20:19:50 +0900

    회원가입 첫번째 페이지 완

--- app/src/main/java/com/team1/valueupapp/activity/SignUpActivity.java
+++ app/src/main/java/com/team1/valueupapp/activity/SignUpActivity.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 /**
  * Created by hyemi on 2015-08-02.
  */
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     EditText user_name;
     EditText mail;
     EditText pass;
@@ -32,15 +32,8 @@ public class SignUpActivity extends AppCompatActivity {
     TextView mail_null;
     TextView pass_null;
     TextView pass_check_null;
-    Button signup_btn;
-    RadioGroup fieldgroup;
-    RadioButton field1;
-    RadioButton field2;
-    RadioButton field3;
-    String job="";
-
+    Button btn_next;
     Toolbar toolbar;
-
     ProgressBar progressBar;
 
     @Override
@@ -58,147 +51,42 @@ public class SignUpActivity extends AppCompatActivity {
         mail = (EditText) findViewById(R.id.mail);
         pass = (EditText) findViewById(R.id.pass);
         pass_check = (EditText) findViewById(R.id.pass_check);
-
         user_name_null = (TextView) findViewById(R.id.user_name_null);
         mail_null = (TextView) findViewById(R.id.mail_null);
         pass_null = (TextView) findViewById(R.id.pass_null);
         pass_check_null = (TextView) findViewById(R.id.pass_check_null);
-
-        signup_btn = (Button) findViewById(R.id.signup_btn);
-
+        btn_next = (Button) findViewById(R.id.btn_next);
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
-        fieldgroup = (RadioGroup) findViewById(R.id.fieldgroup);
-        field1 = (RadioButton) findViewById(R.id.field1);
-        field2 = (RadioButton) findViewById(R.id.field2);
-        field3 = (RadioButton) findViewById(R.id.field3);
 
-        fieldgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.field1:
-                        job = "plan";
-                        break;
-                    case R.id.field2:
-                        job = "dev";
-                        break;
-                    case R.id.field3:
-                        job = "dis";
-                        break;
-                }
-            }
-        });
-
-
-        signup_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (nullCheck() == 0) {
-                                    if (String.valueOf(pass.getText()).equals(String.valueOf(pass_check.getText()))) {
-
-                                        progressBar.setVisibility(View.VISIBLE);
-                                        final ParseUser user = new ParseUser();
-
-                                        user.setUsername(String.valueOf(mail.getText()));
-                                        user.setPassword(String.valueOf(pass.getText()));
-
-                                        user.put("name", String.valueOf(user_name.getText()));
-                                        user.put("job", job);
-//                                        user.put("pick", new ArrayList<>());
-                                        user.put("memo_owner", new ArrayList<>());
-                                        user.put("memo", new ArrayList<>());
-                                        user.put("detail", "");
-                                        user.put("info", "");
-
-
-
-                                        user.signUpInBackground(new SignUpCallback() {
-                                            @Override
-                                            public void done(com.parse.ParseException e) {
-                                                progressBar.setVisibility(View.GONE);
-                                                if (e == null) {
-                                                    Toast.makeText(getApplicationContext(), "가입에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                                                    Intent intent = new Intent(SignUpActivity.this, MyPageEditActivity.class);
-                                                    intent.putExtra("name", String.valueOf(user_name.getText()));
-                                                    intent.putExtra("job", job);
-                                                    intent.putExtra("mydetail", "");
-                                                    intent.putExtra("myinfo", "");
-                                                    intent.putExtra("signup", 1);
-
-                                                    ParseObject object = new ParseObject("Picked");
-                                                    object.put("user", ParseUser.getCurrentUser());
-                                                    object.saveInBackground();
-
-
-                                                    startActivity(intent);
-                                                    finish();
-                                                } else {
-//                                    Log.d("error", String.valueOf(e.getCode()));
-                                                    if (e.getCode() == 202) {
-                                                        mail_null.setText("이미 존재하는 mail 주소입니다.");
-                                                    } else {
-                                                        Toast.makeText(getApplicationContext(), "문제가 발생하였습니다.", Toast.LENGTH_SHORT).show();
-                                                    }//end else
-
-                                                }//end else
-                                            }
-                                        });//user.signUp
-
-                                    } else {
-                                        pass_check_null.setText("비밀번호를 확인하세요.");
-                                    }//end else
-
-                                }//end if
-                            }
-                        });
-                    }
-                }).start();
-
-            }
-        });//signup_btn.clickListner
-
-
+        btn_next.setOnClickListener(this);
     }//onCreate
 
-    public int nullCheck() {
-        int count = 0;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_next:             //다음 버튼이 눌렸을 때
+                if(user_name.getText().length()==0) //이름이 입력되지 않았을 때
+                    Toast.makeText(SignUpActivity.this, "이름을 입력하세요.", Toast.LENGTH_SHORT).show();
+                else if(mail.getText().length()==0) //이메일이 입력되지 않았을 때
+                    Toast.makeText(SignUpActivity.this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
+                else if(pass.getText().length()==0 || pass_check.getText().length()==0) //비밀번호나 확인이 입력되지 않았을 때
+                    Toast.makeText(SignUpActivity.this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
+                else {          //전부 입력되었을 때
+                    if (String.valueOf(pass.getText()).equals(String.valueOf(pass_check.getText()))) {   //비밀번호와 확인이 같을 경우
+                        Intent intent = new Intent(SignUpActivity.this, SignUp_2_Activity.class);
+                        intent.putExtra("email", String.valueOf(mail.getText()));      //이름, email, 비밀번호 SignUp_2_Activity로 전달
+                        intent.putExtra("password", String.valueOf(pass.getText()));
+                        intent.putExtra("name", String.valueOf(user_name.getText()));
 
-        if (String.valueOf(user_name.getText()).equals("")) {
-            user_name_null.setText("항목을 입력하세요.");
-            count++;
-        } else {
-            user_name_null.setText("");
-        }//end else
-
-        if (String.valueOf(mail.getText()).equals("")) {
-            mail_null.setText("항목을 입력하세요.");
-            count++;
-        } else {
-            mail_null.setText("");
-        }//end else
-
-        if (String.valueOf(pass.getText()).equals("")) {
-            pass_null.setText("항목을 입력하세요.");
-            count++;
-        } else {
-            pass_null.setText("");
-        }//end else
-
-        if (String.valueOf(pass_check.getText()).equals("")) {
-            pass_check_null.setText("항목을 입력하세요.");
-            count++;
-        } else {
-            pass_check_null.setText("");
-        }//end else
-
-        return count;
-    }//nullCheck
+                        startActivity(intent);              // SignUp_2_Activity로 이동
+                    }           //endif
+                    else {      //비밀번호와 확인이 다를 경우
+                        Toast.makeText(SignUpActivity.this, "비밀번호를 확인하세요.", Toast.LENGTH_SHORT).show(); //비밀번호 확인 관련 토스트
+                    }       //end else
+                }
+                break;
+        }       //end switch
+    }       //end onClick Method
 
 }//class
 
