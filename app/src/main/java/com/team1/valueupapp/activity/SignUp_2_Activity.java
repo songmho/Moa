3ab620fb389444095eb3a9be6e706d049fc8e786
songmho <songmho@gmail.com>
2016-03-02 01:59:43 +0900

    xml에서 id명 오류나는 부분 수정. SignUp_2_Activity에서 컴파일 오류나는 부분 수정

--- app/src/main/java/com/team1/valueupapp/activity/SignUp_2_Activity.java
+++ app/src/main/java/com/team1/valueupapp/activity/SignUp_2_Activity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.team1.valueupapp.R;
+
 /**
  * Created by songmho on 16. 2. 27.
  */
